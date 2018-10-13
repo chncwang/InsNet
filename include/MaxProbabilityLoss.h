06801439d75925f081a324e7ed74542502c09616
root <chncwang@gmail.com>
2018-10-13 09:58:58 +0800

    loss

--- include/MaxProbabilityLoss.h
+++ include/MaxProbabilityLoss.h
@@ -13,6 +13,7 @@ dtype MaxLogProbabilityLoss(std::vector<Node *> &nodes, const std::vector<int> &
         Node &node = *nodes.at(i);
         dtype max = node.val.v[0];
         for (int j = 1; j < node.dim; ++j) {
+            //std::cout << "j:" << node.val.v[j] << std::endl;
             if (node.val.v[j] > max) {
                 max = node.val.v[j];
             }
@@ -21,10 +22,16 @@ dtype MaxLogProbabilityLoss(std::vector<Node *> &nodes, const std::vector<int> &
         exp.init(node.dim);
         exp.vec() = (node.val.vec() - max).exp();
         dtype sum = static_cast<Eigen::Tensor<dtype, 0>>(exp.vec().sum())(0);
-        node.loss.vec() += exp.vec() / sum;
+        //std::cout << "sum:" << sum << std::endl;
+
+        Tensor1D loss_tensor;
+        loss_tensor.init(node.dim);
+        loss_tensor.vec() = exp.vec() / sum;
         int answer = answers.at(i);
-        node.loss.v[answer] -= 1.0f;
-        node.loss.vec().unaryExpr([=](dtype x)->dtype {return x * reverse_batchsize;});
+        loss_tensor.v[answer] -= 1.0f;
+        node.loss.vec() = loss_tensor.vec().unaryExpr(
+                [=](dtype x)->dtype {return x * reverse_batchsize;});
+        //std::cout << "max:" << max << " answer v:" << node.val.v[answer] << std::endl;
         loss += (log(sum) - node.val.v[answer] + max) * reverse_batchsize;
     }
 
