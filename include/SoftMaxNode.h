0b38f4d4073ef2373c04ebcf368edb833ae1dc68
Chauncey Wang <chncwang@gmail.com>
2021-02-11 15:17:31 +0800

    softmax cpu

--- include/SoftMaxNode.h
+++ include/SoftMaxNode.h
@@ -9,6 +9,76 @@
 
 #include <boost/format.hpp>
 
+class SoftmaxNode : public UniInputNode, public Poolable<SoftmaxNode> {
+public:
+    SoftmaxNode() : UniInputNode("softmax") {}
+
+    void initNode(int dim) override {
+        init(dim);
+    }
+
+    void setNodeDim(int dim) override {
+        setDim(dim);
+    }
+
+    Executor *generate() override;
+
+    void compute() override {
+        dtype max = getInput()->getVal().mat().maxCoeff();
+        Tensor1D x_exp, x;
+        x.init(getDim());
+        x_exp.init(getDim());
+        x.vec() = getInput()->getVal().vec() - max;
+        x_exp.vec() = x.vec().exp();
+        dtype sum = x_exp.mat().sum();
+        val().vec() = x_exp.vec() / sum;
+    }
+
+    void backward() override {
+        Tensor1D a;
+        a.init(getDim());
+        a.vec() = getLoss().vec() * getVal().vec();
+        dtype z = a.mat().sum();
+        Tensor1D b;
+        b.init(getDim());
+        b.vec() = z - a.vec();
+        getInput()->loss().vec() += getVal().vec() *
+            ((1 - getVal().vec()) * getLoss().vec() - b.vec());
+    }
+
+    string typeSignature() const override {
+        return Node::getNodeType();
+    }
+
+protected:
+    virtual bool isDimLegal(const Node &input) const override {
+        return true;
+    }
+};
+
+class BatchedSoftmaxNode : public BatchedNodeImpl<SoftmaxNode> {
+public:
+    void init(Graph &graph, BatchedNode &input) {
+        allocateBatch(input.getDims());
+        setInputsPerNode({&input});
+        afterInit(graph, {&input});
+    }
+};
+
+#if USE_GPU
+#else
+class SoftmaxExecutor : public UniInputExecutor {
+public:
+    int calculateFLOPs() override {
+        return 0; // TODO
+    }
+};
+#endif
+
+Executor *SoftmaxNode::generate() {
+    return new SoftmaxExecutor;
+}
+
 namespace n3ldg_plus {
 
 Node *minusMaxScalar(Graph &graph, Node &input, int input_col) {
@@ -47,18 +117,20 @@ Node* softmax(Graph &graph, Node &input, int input_col) {
 
 BatchedNode* softmax(Graph &graph, BatchedNode &input, int input_col = 1) {
     using namespace n3ldg_plus;
-//    return n3ldg_plus::exp(graph, input);
+    BatchedSoftmaxNode *node = new BatchedSoftmaxNode;
+    node->init(graph, input);
+    return node;
 
-    BatchedNode *subtracted = minusMaxScalar(graph, input, input_col);
-    BatchedNode *exp = n3ldg_plus::exp(graph, *subtracted);
-    BatchedNode *sum = vectorSum(graph, *exp, input_col);
-    vector<int> input_rows;
-    for (int dim : input.getDims()) {
-        input_rows.push_back(dim / input_col);
-    }
-    sum = scalarToVector(graph, *sum, input_rows);
-    BatchedNode *div = n3ldg_plus::fullDiv(graph, *exp, *sum);
-    return div;
+//    BatchedNode *subtracted = minusMaxScalar(graph, input, input_col);
+//    BatchedNode *exp = n3ldg_plus::exp(graph, *subtracted);
+//    BatchedNode *sum = vectorSum(graph, *exp, input_col);
+//    vector<int> input_rows;
+//    for (int dim : input.getDims()) {
+//        input_rows.push_back(dim / input_col);
+//    }
+//    sum = scalarToVector(graph, *sum, input_rows);
+//    BatchedNode *div = n3ldg_plus::fullDiv(graph, *exp, *sum);
+//    return div;
 }
 
 };
