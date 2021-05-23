3b73ac012edad8864e034c357b15a5c2e351529f
daoce <chncwang@gmail.com>
2021-05-23 21:05:49 +0800

    add cat doc

--- include/n3ldg-plus/operator/concat.h
+++ include/n3ldg-plus/operator/concat.h
@@ -5,6 +5,16 @@
 
 namespace n3ldg_plus {
 
+/// \ingroup operator
+/// Concaternate input matrices into the result matrix with a specified column number.
+///
+/// For example, cat({[0.1, 0.2, 0.3, 0.4], [0.1, 0.2, 0.3, 0.4]}) will return [0.1, 0.2, 0.3, 0.4, 0.1, 0.2, 0.3, 0.4], and cat({[0.1, 0.2, 0.3, 0.4], [0.1, 0.2, 0.3, 0.4], 2) will return [0.1, 0.2, 0.1, 0.2, 0.3, 0.4, 0.3, 0.4].
+///
+/// **The operators whose column number and input matrix sizes are equal one by one will be executed in batch.**
+/// For example, cat({[0.1, 0.2, 0.3, 0.4], [0.1], [0.1, 0.2]}) and cat({[0, 0, 0, 0], [0], [0, 0]}) will be executed in batch.
+/// \param inputs The input matrices
+/// \param col The column number of both the input matrices and the result matrix.
+/// \return The result matrix. Its size is equal to the sum of all input matrix sizes.
 Node *cat(const std::vector<Node*> &inputs, int col = 1);
 
 Node *cat(BatchedNode &inputs, int col = 1);
