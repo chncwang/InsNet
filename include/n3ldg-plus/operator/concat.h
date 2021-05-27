d91a03c8464437cc396d8f747cbf2b5af5fe2477
daoce <chncwang@gmail.com>
2021-05-27 23:27:39 +0800

    modify cat doc

--- include/n3ldg-plus/operator/concat.h
+++ include/n3ldg-plus/operator/concat.h
@@ -12,6 +12,9 @@ namespace n3ldg_plus {
 ///
 /// **The operators whose column number and input matrix sizes are equal one by one will be executed in batch.**
 /// For example, cat({[0.1, 0.2, 0.3, 0.4], [0.1], [0.1, 0.2]}) and cat({[0, 0, 0, 0], [0], [0, 0]}) will be executed in batch.
+///
+/// **The operators whose column number is 1 and input tensor sizes are all the same will also be executed in batch.** This rule is especially useful when concaternating RNN hidden states.
+/// For example, cat({[0.1, 0.2], [0.1, 0], [0.1, 0.2]}) and cat({[0, 0], [0, 0]}) will be executed in batch because their input tensors have the same size of 2, though they have different number of input tensors.
 /// \param inputs The input matrices
 /// \param col The column number of both the input matrices and the result matrix.
 /// \return The result matrix. Its size is equal to the sum of all input matrix sizes.
