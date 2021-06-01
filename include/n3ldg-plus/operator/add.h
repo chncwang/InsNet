ed0570ad540c155d50d38c54ff957b812ab059e4
Chauncey Wang <chncwang@gmail.com>
2021-06-01 18:00:25 +0800

    sub doc

--- include/n3ldg-plus/operator/add.h
+++ include/n3ldg-plus/operator/add.h
@@ -8,7 +8,7 @@ namespace n3ldg_plus {
 /// \ingroup operator
 /// Add input tensors.
 ///
-/// **The operators that have the same number of inputs will be executed in batch.** For example, [0.1, 0.1] + [0.2, 0.2] and [0.3, 0.3, 0.3] + [0.4, 0.4, 0.4] will be executed in batch, but [0.1, 0.1] + [0.2, 0.2] and [0.1, 0.1] + [0.2, 0.2] + [0.3, 0.3] will not. If the latter is your case, use *sumPool* instead.
+/// **The add operators that have the same number of inputs will be executed in batch.** For example, [0.1, 0.1] + [0.2, 0.2] and [0.3, 0.3, 0.3] + [0.4, 0.4, 0.4] will be executed in batch, but [0.1, 0.1] + [0.2, 0.2] and [0.1, 0.1] + [0.2, 0.2] + [0.3, 0.3] will not. If the latter is your case, use *sumPool* instead.
 /// \param inputs The input tensors to be added. Note that their sizes should be equal.
 /// \return The sum of the input tensors. Its size is equal to the size of any input tensor.
 Node *add(const std::vector<Node*> &inputs);
--- include/n3ldg-plus/operator/sub.h
+++ include/n3ldg-plus/operator/sub.h
@@ -5,6 +5,12 @@
 
 namespace n3ldg_plus {
 
+/// \ingroup operator
+/// Subtract an input tensor by another.
+///
+/// **All sub operators will be executed in batch.**
+/// \param inputs The input tensors to be added. Note that their sizes should be equal.
+/// \return The sum of the input tensors. Its size is equal to the size of any input tensor.
 Node *sub(Node &minuend, Node &subtrahend);
 
 }
